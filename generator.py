886511ce87024b0201667146b2a90a9da196e114
devenperez <63876261+devenperez@users.noreply.github.com>
2022-06-18 19:53:44 -0400

    Names, numbers, and URLs

--- generator.py
+++ generator.py
@@ -14,6 +14,11 @@ def rmdirAll(path):
             os.remove(fullpath)
     os.rmdir(path)
 
+def get_embed_from_url(url):
+    target = url.replace(" ", "%20")
+    target = url.replace(":", "%3A")
+    target = url.replace("/", "%2F")
+    return "<script src=\"https://emgithub.com/embed.js?target=" + target + "&style=github&showBorder=on&showLineNumbers=on\"></script>"
 
 filesToIgnore = [".git", "README.md"]
 
@@ -26,4 +31,29 @@ git.Repo.clone_from('https://github.com/devenperez/leetcode', "leetcode")
 
 for problemFolder in os.listdir("leetcode"):
     if problemFolder in filesToIgnore: continue #Ignores non-problem folders
-    print(problemFolder)
+    embedCode = ""
+    codeFolder = ""
+    difficulty = ""
+    
+    # Retrieve problem info
+    folderSplit = problemFolder.split("-")
+    number = folderSplit.pop(0)
+    name = " ".join(folderSplit).title()
+    
+    print(number)
+    print(name)
+    
+    # 
+    pfPath = os.path.join("leetcode", problemFolder)
+    for file in os.listdir(pfPath):
+        if file == "NOTES.md": 
+            continue #Notes can be ignored for this
+        elif file == "README.md":
+            readme = open(os.path.join(pfPath, file), "r")
+            print(readme.read())
+        else:
+            codeFolder = problemFolder + "/" + file
+            
+    print(codeFolder)
+    print("\n")
+        
