83b91cb4df678f62f0eb24a920f3b4219e1b76c5
devenperez <63876261+devenperez@users.noreply.github.com>
2022-12-21 21:44:11 -0500

    Added manual utilization

--- generator.py
+++ generator.py
@@ -6,6 +6,8 @@ from problem import Problem
 import json
 
 # Recursive delete folder
+
+
 def rmdirAll(path):
     for file in os.listdir(path):
         fullpath = os.path.join(path, file)
@@ -16,6 +18,7 @@ def rmdirAll(path):
             os.remove(fullpath)
     os.rmdir(path)
 
+
 def strToBool(s):
     if s.lower() == "true":
         return True
@@ -24,106 +27,112 @@ def strToBool(s):
     else:
         raise Exception("Invalid arguments (bool)")
 
+
+def YesOrNoQuestion(question):
+    response = ""
+    yesResponses = ["Y", "y", "yes", "Yes", "YES"]
+    noResponses = ["N", "n", "no", "No", "NO"]
+    validResponses = yesResponses + noResponses
+    while not response in validResponses:
+        response = input(f"{question} (Y/N) ")
+
+    if response in yesResponses:
+        return True
+    return False
+
 ### Start ###
 argsUnparsed = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 args = {
+    "auto": False,      # Automatically executes without any further input
     "leetcode": True,   # Pull new leetcode from GitHub
     "csv": False,       # Create CSV file of all gathered information
     "json": True,       # Upload generated JSON to GitHub (api-git)
-    "debug": False      # Print out validation table 
+    "debug": False      # Print out validation table
 }
 filesToIgnore = [".git", "README.md"]
+global problems
 problems = []
+global easyProblemsSolved
+global mediumProblemsSolved
+global hardProblemsSolved
 easyProblemsSolved = 0
 mediumProblemsSolved = 0
 hardProblemsSolved = 0
 currentTime = int(time.time() * 1000)
 
-# Parse arguments
-if len(argsUnparsed) > 0:
-    if argsUnparsed[0].__contains__("="):
-        # Manual argument assignment
-        for a in argsUnparsed:
-            keyValPair = a.split("=")
-            if len(keyValPair) != 2:
-                raise Exception("Invalid arguments (missing equal sign)")
+def PullFromGithub(shouldFetch):
+    if shouldFetch:
+        # Deletes existing leetcode folder
+        if os.path.exists(os.path.join("repos", "leetcode")):
+            # Pulling from origin (leetcode)
+            print("Pulling new commits from leetcode on GitHub")
+            leetcodeRepo = git.Repo(os.path.join("repos", "leetcode"))
+            leetcodeRepo.remotes.origin.pull()
+            print("Finished pulling new commits from leetcode on GitHub")
+        else:
+            # Clone repo from github
+            print("Cloning new leetcode folder from GitHub")
+            git.Repo.clone_from(
+                'https://github.com/devenperez/leetcode.git', os.path.join("repos", "leetcode"))
+            print("Finished cloning new leetcode folder from GitHub")
+    else:
+        # If not recloning, check if it exists
+        if not os.path.exists(os.path.join("repos", "leetcode")):
+            raise Exception("\"leetcode\" folder does not exist")
+        leetcodeRepo = git.Repo(os.path.join("repos", "leetcode"))
 
-            if keyValPair[0].lower() in args.keys():
-                args[keyValPair[0].lower()] = strToBool(keyValPair[1])
+def GatherInfo():
+    global easyProblemsSolved
+    global mediumProblemsSolved
+    global hardProblemsSolved
+    print("Starting gathering info from repo")
+    numFolders = len(os.listdir(os.path.join("repos", "leetcode")))
+    counter = 0
+    # Gather all necessary info from repo
+    for problemFolder in os.listdir(os.path.join("repos", "leetcode")):
+        counter += 1
+        if problemFolder in filesToIgnore:
+            continue  # Ignores non-problem folders
+
+        print(f"({counter}/{numFolders}) Gathering info for {problemFolder}")
+        # Retrieve problem info
+        folderSplit = problemFolder.split("-")
+        prob = Problem(folderSplit.pop(0), " ".join(folderSplit).title())
+
+        # Gather info from folder
+        pfPath = os.path.join("repos", "leetcode", problemFolder)
+        for file in os.listdir(pfPath):
+            if file == "NOTES.md":
+                pass
+            elif file.startswith("STATS"):
+                with open(os.path.join(pfPath, file), "r") as stats:
+                    wordBreaks = stats.read().split(" ")
+                stats = prob.getOrAddStats(file.split("_")[1].split(".")[0])
+                stats.addStats(float(wordBreaks[1]), float(
+                    wordBreaks[3][1:-3]), float(wordBreaks[5]), float(wordBreaks[7][1:-2]))
+            elif file == "README.md":
+                with open(os.path.join(pfPath, file), "r") as readme:
+                    prob.difficulty = readme.read().split("h3")[1][1:-2]
+                    easyProblemsSolved += 1 if prob.difficulty == "Easy" else 0
+                    mediumProblemsSolved += 1 if prob.difficulty == "Medium" else 0
+                    hardProblemsSolved += 1 if prob.difficulty == "Hard" else 0
             else:
-                raise Exception("Invalid arguments (key)")
-    else:
-        # Sequential argument assignment
-        if len(argsUnparsed) == len(args):
-            args["leetcode"] = strToBool(argsUnparsed[0])
-            args["csv"] = strToBool(argsUnparsed[1])
-            args["json"] = strToBool(argsUnparsed[2])
-            args["debug"] = strToBool(argsUnparsed[3])
-        else:
-            raise Exception("Invalid arguments (num of args)")
+                prob.codeFolder = problemFolder + "/" + file
+                prob.validate(file.split(".")[-1])
 
-# Pull leetcode from GitHub
-if args["leetcode"]:
-    # Deletes existing leetcode folder
-    if os.path.exists(os.path.join("repos","leetcode")):
-        # Pulling from origin (leetcode)
-        print("Pulling new commits from leetcode on GitHub")
-        leetcodeRepo = git.Repo(os.path.join("repos","leetcode"))
-        leetcodeRepo.remotes.origin.pull()
-        print("Finished pulling new commits from leetcode on GitHub")
-    else:
-        # Clone repo from github
-        print("Cloning new leetcode folder from GitHub")
-        git.Repo.clone_from('https://github.com/devenperez/leetcode.git', os.path.join("repos", "leetcode"))
-        print("Finished cloning new leetcode folder from GitHub")
-else:
-    # If not recloning, check if it exists
-    if not os.path.exists(os.path.join("repos","leetcode")):
-        raise Exception("\"leetcode\" folder does not exist")
-    leetcodeRepo = git.Repo(os.path.join("repos","leetcode"))
+        problems.append(prob)
+    print("Finished gathering info from repo")
 
-print("Starting gathering info from repo")
-numFolders = len(os.listdir(os.path.join("repos","leetcode")))
-counter = 0
-# Gather all necessary info from repo
-for problemFolder in os.listdir(os.path.join("repos","leetcode")):
-    counter += 1
-    if problemFolder in filesToIgnore: continue #Ignores non-problem folders
+def SortProblems():   
+    print("Sorting problem list")
+    problems.sort(
+        reverse=True, key=lambda p: p.stats[0].timePercentile + p.stats[0].memoryPercentile)
+    print("Finished sorting problem list")
 
-    print(f"({counter}/{numFolders}) Gathering info for {problemFolder}")
-    # Retrieve problem info
-    folderSplit = problemFolder.split("-")
-    prob = Problem(folderSplit.pop(0), " ".join(folderSplit).title())
+def PrintDebug(shouldPrint):
+    if not shouldPrint:
+        return
 
-    
-    # Gather info from folder
-    pfPath = os.path.join("repos", "leetcode", problemFolder)
-    for file in os.listdir(pfPath):
-        if file == "NOTES.md":
-            pass
-        elif file.startswith("STATS"):
-            with open(os.path.join(pfPath, file), "r") as stats:
-                wordBreaks = stats.read().split(" ")
-            stats = prob.getOrAddStats(file.split("_")[1].split(".")[0])
-            stats.addStats(float(wordBreaks[1]), float(wordBreaks[3][1:-3]), float(wordBreaks[5]), float(wordBreaks[7][1:-2]))
-        elif file == "README.md":
-            with open(os.path.join(pfPath, file), "r") as readme:
-                prob.difficulty = readme.read().split("h3")[1][1:-2]
-                easyProblemsSolved += 1 if prob.difficulty == "Easy" else 0
-                mediumProblemsSolved += 1 if prob.difficulty == "Medium" else 0
-                hardProblemsSolved += 1 if prob.difficulty == "Hard" else 0
-        else:
-            prob.codeFolder = problemFolder + "/" + file
-            prob.validate(file.split(".")[-1])
-    
-    problems.append(prob)
-print("Finished gathering info from repo")
-
-print("Sorting problem list")
-problems.sort(reverse=True, key=lambda p : p.stats[0].timePercentile + p.stats[0].memoryPercentile)
-print("Finished sorting problem list")
-
-if args["debug"]:
     print("-" * 45)
     print("* = Missing code, ' = Missing stats")
     print("-" * 45)
@@ -131,8 +140,11 @@ if args["debug"]:
         print(p.getDebugRow())
     print("-" * 45)
 
-# Create CSV file
-if args["csv"]:
+def CreateCSV(shouldCreate):
+    # Create CSV file
+    if not shouldCreate:
+        return
+
     print("Creating CSV file")
     # Export all info to .csv file
     if os.path.exists("problems_solved.csv"):
@@ -144,7 +156,10 @@ if args["csv"]:
             csv.write("\n")
     print("Finished CSV file")
 
-if args["json"]:
+def CreateJSON(shouldCreate):
+    if not shouldCreate:
+        return
+
     # Pulling new api-git
     if os.path.exists(os.path.join("repos", "api-git")):
         print("Pulling new commits from api-git on GitHub")
@@ -153,11 +168,12 @@ if args["json"]:
         print("Finished pulling new commits from api-git on GitHub")
     else:
         print("Cloning new api-git folder from GitHub")
-        git.Repo.clone_from("https://github.com/devenperez/api-git.git", os.path.join("repos", "api-git"))
+        git.Repo.clone_from(
+            "https://github.com/devenperez/api-git.git", os.path.join("repos", "api-git"))
         apiRepo = git.Repo(os.path.join("repos", "api-git"))
         print("Finished cloning new api-git folder from GitHub")
 
-    ## Writing JSON file
+    # Writing JSON file
     print("Creating JSON file")
     jsonDictionary = {
         "timeGathered": currentTime,
@@ -189,22 +205,92 @@ if args["json"]:
                 "memoryPercentile": pstats.memoryPercentile
             }
             individualProblemDict["languagesSolved"].append(langSolvedSection)
-        individualProblemDict["languagesSolved"].sort(reverse=True, key=lambda p : p["timePercentile"] + p["memoryPercentile"])
+        individualProblemDict["languagesSolved"].sort(
+            reverse=True, key=lambda p: p["timePercentile"] + p["memoryPercentile"])
 
-        jsonDictionary["solvedProblems"]["problems"].append(individualProblemDict)
+        jsonDictionary["solvedProblems"]["problems"].append(
+            individualProblemDict)
 
     jsonContents = json.dumps(jsonDictionary, indent=4)
 
     # Delete existing JSON file
     if os.path.exists(os.path.join("repos", "api-git", "data", "problems_solved.json")):
-        os.remove(os.path.join("repos", "api-git", "data", "problems_solved.json"))
+        os.remove(os.path.join("repos", "api-git",
+                  "data", "problems_solved.json"))
 
     with open(os.path.join("repos", "api-git", "data", "problems_solved.json"), "x") as jsonFile:
         jsonFile.write(jsonContents)
     print("Finished creating JSON file")
 
+def UploadJSON(shouldUpload):
+    if not shouldUpload:
+        return
+
     print("Uploading JSON file to GitHub")
+    apiRepo = git.Repo(os.path.join("repos", "api-git"))
     apiRepo.index.add([os.path.join("data", "problems_solved.json")])
     apiRepo.index.commit(f"Updated problem_solved.json ({currentTime})")
     apiRepo.remotes.origin.push()
     print("Finished uploading JSON file to GitHub")
+
+
+# Parse arguments
+if len(argsUnparsed) > 0:
+    if argsUnparsed[0].__contains__("="):
+        # Manual argument assignment
+        for a in argsUnparsed:
+            keyValPair = a.split("=")
+            if len(keyValPair) != 2:
+                raise Exception("Invalid arguments (missing equal sign)")
+
+            if keyValPair[0].lower() in args.keys():
+                args[keyValPair[0].lower()] = strToBool(keyValPair[1])
+            else:
+                raise Exception("Invalid arguments (key)")
+    else:
+        # Sequential argument assignment
+        if len(argsUnparsed) == len(args):
+            args["auto"] = strToBool(argsUnparsed[0])
+            args["leetcode"] = strToBool(argsUnparsed[1])
+            args["csv"] = strToBool(argsUnparsed[2])
+            args["json"] = strToBool(argsUnparsed[3])
+            args["debug"] = strToBool(argsUnparsed[4])
+        else:
+            raise Exception("Invalid arguments (num of args)")
+# Manual interpretation
+if not args["auto"]:
+    print("""
+.__                 __                   .___                 
+                              
+|  |   ____   _____/  |_  ____  ____   __| _/____   ___________ _______  ______ ___________ 
+|  | _/ __ \_/ __ \   __\/ ___\/  _ \ / __ |/ __ \  \____ \__  \\_  __ \/  ___// __ \_  __ \\
+|  |_\  ___/\  ___/|  | \  \__(  <_> ) /_/ \  ___/  |  |_> > __ \|  | \/\___ \\  ___/|  | \/
+|____/\___  >\___  >__|  \___  >____/\____ |\___  > |   __(____  /__|  /____  >\___  >__|   
+          \/     \/          \/           \/    \/  |__|       \/           \/     \/       
+    """)
+    print("Welcome to Leetcode parser. This application takes your leetcode github repository and compresses into different file formats.")
+    print("Of course, you knew that already because this is a very specified program that only works with your github repo.")
+    print("\n"*3)
+    PullFromGithub(YesOrNoQuestion(
+        "Would you like to pull the newest version of the repo from GitHub?"))
+    GatherInfo()
+    SortProblems()
+    print(problems)
+    PrintDebug(YesOrNoQuestion(
+        "Would you like to print the debug table?"))
+    CreateCSV(YesOrNoQuestion(
+        "Would you like to create the CSV file?"))
+    CreateJSON(YesOrNoQuestion(
+        "Would you like to create the JSON file?"))    
+    UploadJSON(YesOrNoQuestion(
+        "Would you like to upload this JSON file?"))
+    exit()
+
+#### AUTO VERSION #####
+PullFromGithub(args["leetcode"])
+GatherInfo()
+SortProblems()
+PrintDebug(args["debug"])
+CreateCSV(args["csv"])
+CreateJSON(args["json"])
+UploadJSON(args["json"])
